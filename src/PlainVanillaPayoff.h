4a99f86cf1147895ec609df7040e5bd230c153bd
andreaR <-->
2020-12-12 11:08:18 +0100

    Restyled PlainVanillaPayoff class
    The sign to be used in the payoff evaluation is precomputed at creation. The implementation is in
    the header file, thus (possibly) leading to efficient inlining.

--- src/PlainVanillaPayoff.h
+++ src/PlainVanillaPayoff.h
@@ -1,15 +1,23 @@
 #ifndef _PLAIN_VANILLA_PAYOFF_H_
 #define _PLAIN_VANILLA_PAYOFF_H_
 
+#include<algorithm>
+
 #include "CallPut.h"
 
 class PlainVanillaPayoff {
 	public:
-		PlainVanillaPayoff(const CallPut& callPut, double strike);
-		double operator() (double S) const;
+		PlainVanillaPayoff(
+			const CallPut& callPut,
+			double strike
+		) : m_call_put_sign{callPut == CallPut::CALL ? 1 : -1}, m_strike{strike} {}
+
+		double operator() (double S) const {
+			return std::max(0.0, m_call_put_sign * (S - m_strike));
+		}
 
 	private:
-		CallPut m_call_put;
+		int m_call_put_sign;
 		double m_strike;
 };
 
